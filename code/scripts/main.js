534a87fa695607f86be2ed25db74a64a8383ed86
av2049 <alexvurbanov@gmail.com>
2024-09-09 17:36:22 +0100

    refactor: moved index page to root

--- code/scripts/main.js
+++ code/scripts/main.js
@@ -103,13 +103,13 @@ main();
 
 // Add an event listeners to navigation buttons
 document.getElementById('ldavis-button').addEventListener('click', function() {
-    window.location.href = 'html/ldavis_dashboard.html';
+    window.location.href = '/code/html/ldavis_dashboard.html';
 });
 document.getElementById('control-button').addEventListener('click', function() {
-    window.location.href = 'html/control_dashboard.html';
+    window.location.href = '/code/html/control_dashboard.html';
 });
 document.getElementById('buuble-map-button').addEventListener('click', function() {
-    window.location.href = 'html/bubble_map_dashboard.html';
+    window.location.href = '/code/html/bubble_map_dashboard.html';
 });
 
 
